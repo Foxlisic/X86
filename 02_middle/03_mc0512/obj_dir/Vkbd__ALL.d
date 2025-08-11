69457bf9aa649ff80ac392dbe5fd32fe410d0e75
VGamanin <hplus.foxtail@gmail.com>
2025-08-11 08:34:33 +0300

    DEL

--- 02_middle/03_mc0512/obj_dir/Vkbd__ALL.d
+++
@@ -1,9 +0,0 @@
-Vkbd__ALL.o: Vkbd__ALL.cpp Vkbd.cpp Vkbd__pch.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilatedos.h \
- /usr/local/share/verilator/include/verilated_config.h \
- /usr/local/share/verilator/include/verilated_types.h \
- /usr/local/share/verilator/include/verilated_funcs.h Vkbd__Syms.h Vkbd.h \
- Vkbd___024root.h Vkbd___024root__DepSet_h6dbf1644__0.cpp \
- Vkbd___024root__DepSet_h5421c56a__0.cpp Vkbd___024root__Slow.cpp \
- Vkbd___024root__DepSet_h5421c56a__0__Slow.cpp Vkbd__Syms.cpp
